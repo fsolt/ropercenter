d5d1ee7d1f911f0af426374d033f261421f64188
Frederick Solt <fsolt@mac.com>
2017-02-28 15:00:16 -0600

    use RSelenium (minimal working, it seems)

--- R/roper_download.R
+++ R/roper_download.R
@@ -1,94 +1,135 @@
 #' Download datasets from the Roper Center
 #'
-#' \code{roper_download} provides a programmatic and reproducible means to download datasets from the Roper Center 
+#' \code{roper_download} provides a programmatic and reproducible means to download datasets 
+#'   from the Roper Center data archive
 #'
 #' @param file_id The unique identifier (or optionally a vector of these identifiers)
 #'  for the dataset(s) to be downloaded (see details).
-#' @param email,password Your ICPSR email and password (see details)
+#' @param email,password Your Roper Center email and password (see details)
+#' @param reset If TRUE, you will be asked to re-enter your Roper Center email and password.
 #' @param download_dir The directory (relative to your working directory) to
-#'   which files from the Pew Research Center will be downloaded.
+#'   which files from the Roper Center will be downloaded.
 #' @param msg If TRUE, outputs a message showing which data set is being downloaded.
 #' @param unzip If TRUE, the downloaded zip files will be unzipped.
 #' @param delete_zip If TRUE, the downloaded zip files will be deleted.
 #'
 #' @details 
 #'  To avoid requiring others to edit your scripts to insert their own email and  
-#'  password, the default is set to fetch this information from the user's 
-#'  .Rprofile.  Before running \code{icpsr_download}, then, you should be sure to
-#'  add these options to your .Rprofile substituting your info for the example below:
+#'  password or to force them to do so interactively, the default is set to fetch 
+#'  this information from the user's .Rprofile.  Before running \code{roper_download}, 
+#'  then, you should be sure to add these options to your .Rprofile substituting your 
+#'  info for the example below:
 #'
 #'  \code{
-#'   options("icpsr_email" = "juanita-herrara@uppermidwest.edu",
-#'          "icpsr_password" = "password123!")
+#'   options("roper_email" = "juanita-herrara@uppermidwest.edu",
+#'          "roper_password" = "password123!")
 #'  }
 #'
 #' @return The function returns downloaded files.
 #'
 #' @examples
 #' \dontrun{
-#'  icpsr_download(file_id = c(3730, 36138))
+#'  roper_download(file_id = c("CNCIPO1996-96010", "CNCIPO2000-02"))
 #' }
-#' @importFrom gesis gesis_setup
-#'
+#' 
+#' @import RSelenium
+#' @importFrom stringr str_detect
+#' 
 #' @export
-icpsr_download <- function(file_id, 
-                         email = getOption("icpsr_email"),
-                         password = getOption("icpsr_password"),
-                         download_dir = "icpsr_data",
-                         msg = TRUE,
-                         unzip = TRUE,
-                         delete_zip = TRUE) {
+roper_download <- function(file_id, 
+                           email = getOption("roper_email"),
+                           password = getOption("roper_password"),
+                           reset = FALSE,
+                           download_dir = "roper_data",
+                           msg = TRUE) {
+  
+  # Detect login info
+  if (reset){
+    email <- password <- NULL
+  }
+  
+  if (is.null(email)){
+    roper_email <- readline(prompt = "The Roper Center requires your user account information.  Please enter your email address: \n")
+    options("roper_email" = roper_email)
+    email <- getOption("roper_email")
+  }
+  
+  if (is.null(password)){
+    roper_password <- readline(prompt = "Please enter your Roper Center password: \n")
+    options("roper_password" = roper_password)
+    password <- getOption("roper_password")
+  }
+  
+  # Initialize driver
+  fprof <- makeFirefoxProfile(list(
+    browser.download.dir = file.path(getwd(), download_dir),
+    browser.download.folderList = 2L,
+    browser.download.manager.showWhenStarting = FALSE,
+    pdfjs.disabled=TRUE,
+    plugin.scan.plid.all = FALSE,
+    plugin.scan.Acrobat = "99.0",
+    browser.helperApps.neverAsk.saveToDisk = "application/x-zip, application/por, application/pdf"))
+  rD <- rsDriver(browser = "firefox", extraCapabilities = fprof, verbose = FALSE)
+  remDr <- rD[["client"]]
 
-  quietly(gesis::setup_gesis(download_dir = download_dir,
-                             file_mime = "application/zip; application/octet-stream; application/pdf"))
-  
-  # Get list of current download directory contents
-  if (!dir.exists(download_dir)) dir.create(download_dir, recursive = TRUE)
-  dd_old <- list.files(download_dir)
-  
+  # Sign in
+  signin <- "http://ropercenter.cornell.edu/CFIDE/cf/action/login/signin.cfm"
+  remDr$navigate(signin)
+  remDr$findElement(using = "name", "username")$sendKeysToElement(list(email))
+  remDr$findElement(using = "name", "password")$sendKeysToElement(list(password))
+  remDr$findElement(using = "name", "signin")$clickElement()
+  Sys.sleep(5)
+    
   # Loop through files
   for (i in seq(file_id)) { 
       item <- file_id[[i]]
       if(msg) message("Downloading Roper Center file: ", item, sprintf(" (%s)", Sys.time()))
-      
+
+      # Get list of current download directory contents
+      if (!dir.exists(download_dir)) dir.create(download_dir, recursive = TRUE)
+      dd_old <- list.files(download_dir)
+            
       # build url
-      url <- paste0("http://www.icpsr.umich.edu/cgi-bin/terms?path=ICPSR&study=", item)
+      url <- paste0("http://ropercenter.cornell.edu/CFIDE/cf/action/catalog/abstract.cfm?type=&start=&id=&archno=", item, "&abstract=")
       
-      # navigate to download page
+      # navigate to download page, start download
       remDr$navigate(url)
-      
-      # agree to terms
-      remDr$findElement(using = "name", ".submit")$clickElement()
-      
-      # log in
-      if (i == 1) { 
-          Sys.sleep(2)
-          remDr$findElement(using = "name", "email")$sendKeysToElement(list(email))
-          remDr$findElement(using = "name","password")$sendKeysToElement(list(password))
-          remDr$findElement(using = "name", "Log In")$clickElement()         
-      }
+      remDr$findElement(using = "partial link text", "SPSS file")$clickElement()
       
       # check that download has completed
-      file_id_name <- item 
-      while (nchar(file_id_name) < 5) file_id_name <- paste0("0", file_id_name) # pad out with zeroes as needed
-      zip_name <- paste0("ICPSR_", file_id_name, ".zip")
       dd_new <- list.files(download_dir)[!list.files(download_dir) %in% dd_old]
-      while (!zip_name %in% dd_new) {
+      wait <- TRUE
+      tryCatch(
+        while(all.equal(str_detect(dd_new, "\\.part$"), logical(0))) {
           Sys.sleep(1)
-          dd_new <- list.files(download_dir)[!list.files(download_dir) %in% dd_old]          
+          dd_new <- list.files(download_dir)[!list.files(download_dir) %in% dd_old]
+        }, error = function(e) 1 )
+      while(any(str_detect(dd_new, "\\.part$"))) {
+        Sys.sleep(1)
+        dd_new <- list.files(download_dir)[!list.files(download_dir) %in% dd_old]
       }
-
-      # switch back to first window
-      remDr$switchToWindow(remDr$getWindowHandles()[[1]])
+      
+      # get codebook
+      dd_old <- list.files(download_dir)
+      remDr$findElement(using = "partial link text", "PDF file")$clickElement()
+      
+      # check that download has completed
+      dd_new <- list.files(download_dir)[!list.files(download_dir) %in% dd_old]
+      wait <- TRUE
+      tryCatch(
+        while(all.equal(str_detect(dd_new, "\\.part$"), logical(0))) {
+          Sys.sleep(1)
+          dd_new <- list.files(download_dir)[!list.files(download_dir) %in% dd_old]
+        }, error = function(e) 1 )
+      while(any(str_detect(dd_new, "\\.part$"))) {
+        Sys.sleep(1)
+        dd_new <- list.files(download_dir)[!list.files(download_dir) %in% dd_old]
+      }
+      
   }
   
   # Close driver
   remDr$close()
-  
-  if (unzip == TRUE) {
-    lapply(dd_new, function(x) unzip(paste0(download_dir, "/", x), exdir = paste0(download_dir, "/")))
-  }
-  if (delete_zip == TRUE) {
-    invisible(file.remove(paste0(download_dir, "/", dd_new)))
-  }
-}
+  rD[["server"]]$stop()
+}  
+
